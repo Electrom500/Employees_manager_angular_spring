a384585bbea26d15e4ca2da1f30b9d028a02e8fa
Damien SANTERNE <damien.santerne@gmail.com>
2025-06-24 18:57:36 +0200

    lien angular spring boot fonctionnel

--- employeemanagerapp/src/app/app.ts
+++ employeemanagerapp/src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  //imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'employeemanagerapp';
+export class App implements OnInit {
+  public employees: Employee[] = [];
+
+  constructor(private employeeService:EmployeeService) { }
+  
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees():void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[])=> {
+        this.employees = response;
+      },
+      (error:HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    )
+  }
 }
